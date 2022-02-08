8863b8b6f14d1748bdefe8682fb3724186eed6b6
ENOCH YEOM <hykqo@naver.com>
2022-02-09 00:50:53 +0900

    coding test part 1

---
+++ src/com/company/Cat.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Cat {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
