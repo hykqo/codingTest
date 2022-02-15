827e9dff26e1856a4d95d2ee3ddddde7170f3dcc
ENOCH YEOM <hykqo@naver.com>
2022-02-16 01:01:40 +0900

    2/16

---
+++ src/com/company/ADivB.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class ADivB {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Double a = sc.nextDouble();
+        Double b = sc.nextDouble();
+        Double result = a/b;
+        System.out.println(result);
+    }
+}
