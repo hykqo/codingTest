4dfe1d0f9a220eb9ea97d0427002d812328a19fd
ENOCH YEOM <hykqo@naver.com>
2022-05-25 23:19:28 +0900

    Scanner1

---
+++ src/com/company/Joones.java
@@ -0,0 +1,13 @@
+package com.company;
+import java.util.Scanner;
+
+public class Joones {
+
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String id = scanner.next();
+        System.out.println(id+"??!");
+    }
+}
