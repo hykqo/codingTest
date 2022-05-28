e11cae9698559e3470938403981b3da6ba103e81
ENOCH YEOM <hykqo@naver.com>
2022-05-28 23:22:17 +0900

    합

---
+++ src/com/company/conditional/Plus.java
@@ -0,0 +1,24 @@
+package com.company.conditional;
+/*문제
+n이 주어졌을 때, 1부터 n까지 합을 구하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 n (1 ≤ n ≤ 10,000)이 주어진다.
+
+출력
+1부터 n까지 합을 출력한다.*/
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Plus {
+
+    public static void main(String[] args)throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        if(n < 1 || n > 10000) throw new Exception("n (1 ≤ n ≤ 10,000)");
+        int res = 0;
+        for(int i = 1; i <= n; i++) res = res + i;
+        System.out.println(res);
+    }
+}
