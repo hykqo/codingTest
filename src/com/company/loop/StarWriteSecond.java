70a6c4604eb3a7ed7647c725166feb12cbe28661
ENOCH YEOM <hykqo@naver.com>
2022-06-01 23:40:04 +0900

    별찍기 -1 다른 방법

---
+++ src/com/company/loop/StarWriteSecond.java
@@ -0,0 +1,27 @@
+package com.company.loop;
+/*문제
+첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
+
+입력
+첫째 줄에 N(1 ≤ N ≤ 100)이 주어진다.
+
+출력
+첫째 줄부터 N번째 줄까지 차례대로 별을 출력한다.*/
+
+import java.io.*;
+
+public class StarWriteSecond {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n  = Integer.parseInt(br.readLine());
+        String star = "*";
+        br.close();
+        for(int i =1; i<=n; i++){
+            bw.write(star+"\n");
+            star += "*";
+        }
+        bw.flush();
+        bw.close();
+    }
+}
