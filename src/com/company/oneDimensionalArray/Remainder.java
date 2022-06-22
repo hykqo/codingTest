5869dfef1376547b2e0cc6c2cc39ff700f356abe
ENOCH YEOM <hykqo@naver.com>
2022-06-22 13:39:45 +0900

    1차원 배열 ox퀴즈

--- src/com/company/oneDimensionalArray/Remainder.java
+++ src/com/company/oneDimensionalArray/Remainder.java
@@ -16,7 +16,7 @@ import java.util.Arrays;
 출력
 첫째 줄에, 42로 나누었을 때, 서로 다른 나머지가 몇 개 있는지 출력한다
 */
-public class remainder {
+public class Remainder {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int[] nList = new int[10];
