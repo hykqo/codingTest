ed99faf7c4bbf6eff7b14752e49233f934700df9
ENOCH YEOM <hykqo@naver.com>
2026-03-30 11:56:44 +0900

    버블정렬 복습

---
+++ src/review1/sorting/BubbleSorting2.java
@@ -0,0 +1,27 @@
+package review1.sorting;
+
+import java.util.Scanner;
+
+public class BubbleSorting2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n =  scanner.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = scanner.nextInt();
+        }
+
+        for(int i=0; i<n-1; i++){
+            for(int j=i+1; j<n; j++){
+                if(arr[i] > arr[j]){
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        for(int a : arr){
+            System.out.print(a+ " ");
+        }
+    }
+}
