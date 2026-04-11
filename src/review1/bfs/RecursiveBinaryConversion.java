64f31d709bba3e004635e62843014dc4b54f0a12
ENOCH YEOM <hykqo@naver.com>
2026-04-11 16:20:24 +0900

    이진수 변환(재귀함수)

---
+++ src/review1/bfs/RecursiveBinaryConversion.java
@@ -0,0 +1,19 @@
+package review1.bfs;
+
+import java.util.Scanner;
+
+public class RecursiveBinaryConversion {
+    public static void DFS(int n){
+        if(n == 0) return;
+        DFS(n/2);
+        int x = n % 2;
+        System.out.print(x);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        DFS(n);
+
+    }
+}
