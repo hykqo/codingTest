2426e51865bd077a462a5b8c8d6647772e042352
ENOCH YEOM <hykqo@naver.com>
2026-08-07 10:48:12 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 7.90 ms, Memory: 85.3 MB -BaekjoonHub

--- 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
@@ -1,51 +1,14 @@
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 
 class Solution {
-public int[][] solution(int[][] data, String ext, int val_ext, String sort_by) {
-        int[][] answer = {};
-        int[][] search = search(data, ext, val_ext);
-        sort(search, sort_by);
-        return search;
-    }
-
-    public int[][] search(int[][] data, String ext, int val_ext){
-        List<int[]> answer = new ArrayList<>();
-        int idx = getIdx(ext);
-        if(idx == -1) return null;
-        for (int i=0; i<data.length; i++){
-            if(data[i][idx] < val_ext){
-                answer.add(data[i]);
-            }
-        }
-        return answer.toArray(new int[answer.size()][]);
-    }
-
-
-    public void sort(int[][] data, String sort_by){
-        int idx = getIdx(sort_by);
-        if(idx == -1) return;
-        for(int i=0; i<data.length; i++){
-            for(int j=i+1; j<data.length; j++){
-                if(data[i][idx] > data[j][idx]){
-                    int[] tmp = data[j];
-                    data[j] = data[i];
-                    data[i] = tmp;
-                }
-            }
-        }
-    }
-
-    public int getIdx(String by){
-        if("code".equals(by)){
-            return 0;
-        }else if("date".equals(by)) {
-            return 1;
-        }else if("maximum".equals(by)) {
-            return 2;
-        }else if("remain".equals(by)) {
-            return 3;
-        } else return -1;
+    public int[][] solution(int[][] data, String ext, int val_ext, String sort_by) {
+        String[] s = {"code","date","maximum","remain"};
+        List<String> columnList = Arrays.asList(s);
+        int columnIdx = columnList.indexOf(ext);
+        int sortIdx = columnList.indexOf(sort_by);
+        return Arrays.stream(data).filter(d -> d[columnIdx] < val_ext)
+                .sorted((d1,d2) -> d1[sortIdx] - d2[sortIdx]).toArray(int[][]::new);
     }
 }
