9b06f2d4d325ff2e38b6ce24a4fc94872b6ad20c
742221 <hykqo@naver.com>
2026-08-07 10:49:16 +0900

    PCCE 기출문제: 데이터 분석

---
+++ src/prorammers/level1/dataAnalysis.java
@@ -0,0 +1,16 @@
+package prorammers.level1;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class dataAnalysis {
+
+    public int[][] solution(int[][] data, String ext, int val_ext, String sort_by) {
+        String[] s = {"code","date","maximum","remain"};
+        List<String> columnList = Arrays.asList(s);
+        int columnIdx = columnList.indexOf(ext);
+        int sortIdx = columnList.indexOf(sort_by);
+        return Arrays.stream(data).filter(d -> d[columnIdx] < val_ext)
+                .sorted((d1,d2) -> d1[sortIdx] - d2[sortIdx]).toArray(int[][]::new);
+    }
+}
