874bfa9f0f2e6e6d8da901376db74778a6cedd21
742221 <hykqo@naver.com>
2026-08-10 09:08:53 +0900

    스킬체크: hash-(완주하지 못한 선수)

---
+++ src/prorammers/level1/hash/PlayerWhoDidNotFinish.java
@@ -0,0 +1,25 @@
+package prorammers.level1.hash;
+
+import java.util.HashMap;
+
+public class PlayerWhoDidNotFinish {
+    public String solution(String[] participant, String[] completion) {
+        HashMap<String, Integer> map = new HashMap<>();
+        for (String s : participant) {
+            map.put(s, map.getOrDefault(s, 0)+1);
+        }
+
+        for (String s : completion) {
+            if(map.get(s) == 1) {
+                map.remove(s);
+            } else {
+                map.put(s, map.get(s)-1);
+            }
+        }
+        String answer = "";
+        for (String s : map.keySet()) {
+            answer = s;
+        }
+        return answer;
+    }
+}
