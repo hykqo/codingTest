9a01b7d0080c6791975ff20d6f033d89e4e16499
742221 <hykqo@naver.com>
2026-08-25 09:37:17 +0900

    연습문제: 바탕화면 정리

---
+++ src/review1/array/DesktopCleanUp.java
@@ -0,0 +1,72 @@
+package review1.array;
+
+import java.util.Scanner;
+/*
+3
+.#...
+..#..
+...#.
+
+5
+..........
+.....#....
+......##..
+...##.....
+....#.....
+
+*/
+public class DesktopCleanUp {
+
+   static class Coord {
+        int x;
+        int y;
+
+        public Coord(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+
+        public void setTopLeft(int x, int y){
+            if(this.x > x) {
+                this.x = x;
+            }
+            if(this.y > y) {
+                this.y = y;
+            }
+        }
+
+
+        public void setBottomRight(int x, int y){
+            if(this.x < x) {
+                this.x = x;
+            }
+            if(this.y < y) {
+                this.y = y;
+            }
+        }
+
+    }
+
+    public static int[] solution(String[] wallpaper) {
+        Coord topLeft = new Coord(Integer.MAX_VALUE, Integer.MAX_VALUE);
+        Coord bottomRight = new Coord(Integer.MIN_VALUE, Integer.MIN_VALUE);
+        for (int i = 0; i < wallpaper.length; i++){
+            for (int j = 0; j < wallpaper[i].length(); j++){
+                if(wallpaper[i].charAt(j) == '#') {
+                     topLeft.setTopLeft(i, j);
+                     bottomRight.setBottomRight(i+1, j+1);
+                }
+            }
+        }
+        return new int[]{topLeft.x, topLeft.y, bottomRight.x, bottomRight.y};
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        String[] wallpaper = new String[n];
+        for(int i=0; i<n; i++) wallpaper[i] = scanner.next();
+        System.out.println(solution(wallpaper));
+    }
+}
